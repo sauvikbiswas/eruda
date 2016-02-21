0afc5aba76ff961a425ca38adfe188122c6d51c0
Sauvik Biswas <sauvik@sauvikbiswas.com>
2016-02-21 19:09:24 +0530

    Formalising Tokenizer

--- parser/__init__.py
+++ parser/__init__.py
@@ -5,11 +5,18 @@
 
 from pprint import pprint as pp
 
-class regexops(object):
+class tokenizer(object):
 	
-	def __init__(self, filename, abbrevfile = ''):
-		import re
+	def __init__(self, filename = '', abbrevfile = ''):
 		self.subvector = []
+		if filename.strip() != '':
+			self.add_subvect(filename)
+		if abbrevfile.strip() != '':
+			self.add_abbrev(abbrevfile)
+
+	def add_subvect(self, filename):
+		'''Reads an list of substitution vectors'''
+		import re
 		with open(filename, 'rU') as fp:
 			data = fp.read().split('\n')
 		findflag = False
@@ -26,12 +33,13 @@ class regexops(object):
 		return
 
 	def sub(self, data):
+		'''Runs the substitution vector on data'''
 		for subre, findstr, replacestr in self.subvector:
 			data = subre.sub(replacestr, data)
-			print data
 		return data
 		
 	def add_abbrev(self, abbrevfile):
+		'''Computes the contraction of period and adds them to the substitution vector'''
 		import re
 		with open(abbrevfile, 'rU') as fp:
 			data = fp.read().split('\n')
@@ -45,29 +53,6 @@ class regexops(object):
 			if item.strip() != '']
 		
 
-class parser(object):
-	def __init__(self, filename=''):
-		self.raw = ''
-		self.parsed = []
-		
-		if filename != '':
-			self.update(filename)
-		return
 
-	def update(self, data):
-		'''parser.update(filename) updates the data with that of a file'''
-		self.raw += data
-		
-		# This is a tricky part from an implementation standpoint.
-		# How it has to be parsed is something that must come from
-		# a predefined set of rules.
-	
-		# Step 1: Condition the data using a vector of regexp subs
-		
-		self.parsed = [line.split() for line in raw.split('\n')]
-		return
-
-x = regexops('pre_tokenizer.regexp')
-pp(x.add_abbrev('abbreviation.list'))
-pp(x.subvector)
+x = tokenizer('pre_tokenizer.regexp', 'abbreviation.list')
 pp(x.sub('Mr. and Mrs. Smith went to Washington with I.O.U. their kids. Their kids got lost! ken. mccoy filename.java is a file. 999-888-999 is a number, 55.980 is also a number, so is .999'))
